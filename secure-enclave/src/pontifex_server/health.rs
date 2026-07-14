bb14809d66e728bcaa17afd2bbab028bb6037a47
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2026-07-14 20:28:34 +0000

    refactor(enclave): tighten health operation

--- secure-enclave/src/pontifex_server/health.rs
+++ secure-enclave/src/pontifex_server/health.rs
@@ -3,14 +3,3 @@ use enclave_types::{EnclaveError, HealthRequest};
 pub async fn handler((): (), _: HealthRequest) -> Result<(), EnclaveError> {
     Ok(())
 }
-
-#[cfg(test)]
-mod tests {
-    use super::handler;
-    use enclave_types::HealthRequest;
-
-    #[tokio::test]
-    async fn health_succeeds_when_server_is_running() {
-        assert_eq!(handler((), HealthRequest).await, Ok(()));
-    }
-}
