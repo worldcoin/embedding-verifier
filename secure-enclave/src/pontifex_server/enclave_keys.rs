6f22e0637280b086cdabaf7bce22c7f772646581
kilianglas <glas_k@yahoo.de>
2026-08-18 18:30:18 +0200

    chore: cleanup

--- secure-enclave/src/pontifex_server/enclave_keys.rs
+++ secure-enclave/src/pontifex_server/enclave_keys.rs
@@ -29,22 +29,7 @@ mod tests {
     use enclave_types::{EnclaveError, GetEnclaveKeysRequest};
 
     use super::handler;
-    use crate::test_support::{EchoAttestor, FailingAttestor, state_with};
-
-    #[tokio::test]
-    async fn handler_attests_each_key_separately() {
-        let state = state_with(Arc::new(EchoAttestor));
-
-        let response = handler(Arc::clone(&state), GetEnclaveKeysRequest)
-            .await
-            .expect("both keys should be attested");
-
-        assert_eq!(
-            response.encryption_key_attestation,
-            state.encryption_public_key()
-        );
-        assert_eq!(response.signing_key_attestation, state.signing_public_key());
-    }
+    use crate::test_support::{FailingAttestor, state_with};
 
     #[tokio::test]
     async fn handler_propagates_attestation_failures() {
